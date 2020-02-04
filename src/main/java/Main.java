58fa0030ff721db0fec314af1beb28654b43c63b
Tobias <tms.kleinert@gmx.de>
2020-02-04 21:23:51 +0100

    some adjustments for better quality of code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        //register Modules here
         BigDiscordBot.getInstance().registerModule(twitch_api.ModuleAPI.class);
+
+
+        //start bot
         BigDiscordBot.getInstance().startBot();
     }
 
